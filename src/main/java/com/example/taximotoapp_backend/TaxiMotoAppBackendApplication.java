6894264bac7d266c686b11594e112c6c0841b1c5
Wassim Touil <153998360+Wassimtouil@users.noreply.github.com>
2026-02-12 14:39:06 +0100

    Frist commit

---
+++ src/main/java/com/example/taximotoapp_backend/TaxiMotoAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.taximotoapp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiMotoAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaxiMotoAppBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/taximotoapp_backend/TaxiMotoAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taximotoapp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxiMotoAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
