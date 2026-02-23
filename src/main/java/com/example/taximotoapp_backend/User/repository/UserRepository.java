1ff30a6690ce7970329aa4c1c24fcf4dc59989f5
Wassim Touil <153998360+Wassimtouil@users.noreply.github.com>
2026-02-23 17:25:55 +0100

    add package security inclus JwtService.java

--- src/main/java/com/example/taximotoapp_backend/User/repository/UserRepository.java
+++ src/main/java/com/example/taximotoapp_backend/User/repository/UserRepository.java
@@ -9,10 +9,7 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Integer> {
     Optional<User> findByEmail(String email);
-
     Optional<User> findByFirebaseUid(String firebaseUid);
-
     List<User> findByRole(Role role);
-
     Boolean existsByEmail(String email);
 }
---
+++ src/main/java/com/example/taximotoapp_backend/security/JwtService.java
@@ -0,0 +1,36 @@
+package com.example.taximotoapp_backend.security;
+
+import com.example.taximotoapp_backend.User.model.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+
+@Service
+public class JwtService {
+    private final String SECRET = "mototaxi-secret-key-must-be-long-enough-123456";
+    private Key getSignKey() {
+        return Keys.hmacShaKeyFor(SECRET.getBytes());
+    }
+    public String generateToken(User user) {
+        return Jwts.builder()
+                .setSubject(user.getEmail())
+                .claim("role", user.getRole().name())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
+                .signWith(getSignKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+}
