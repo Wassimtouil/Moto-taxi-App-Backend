c02188f3414963a036fe2640ffdc22b3fabf58e0
Wassim Touil <153998360+Wassimtouil@users.noreply.github.com>
2026-02-24 14:46:51 +0100

    add SecurityConfig

---
+++ src/main/java/com/example/taximotoapp_backend/security/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.example.taximotoapp_backend.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final JwtFilter jwtFilter;
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        // Public
+                        .requestMatchers("/api/auth/**").permitAll()
+
+                        // Role CLIENT
+                        .requestMatchers("/api/trajets/**").hasRole("CLIENT")
+
+                        // Role CHAUFFEUR
+                        .requestMatchers("/api/chauffeur/**").hasRole("CHAUFFEUR")
+
+                        // ADMIN
+                        //.requestMatchers("/api/admin/**").hasRole("ADMIN")
+
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtFilter,
+                        org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+}
