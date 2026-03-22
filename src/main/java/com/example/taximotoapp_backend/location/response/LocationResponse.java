c001d2efa44292fca45357efaf573071895a6c27
Wassim Touil <153998360+Wassimtouil@users.noreply.github.com>
2026-03-22 10:03:52 +0100

    add LocationService function updateLocation for chauffeur

--- src/main/java/com/example/taximotoapp_backend/location/response/LocationResponse.java
+++ src/main/java/com/example/taximotoapp_backend/location/response/LocationResponse.java
@@ -1,8 +1,10 @@
 package com.example.taximotoapp_backend.location.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class LocationResponse {
     private Double latitude;
     private Double longitude;
--- src/main/java/com/example/taximotoapp_backend/location/service/LocationService.java
+++ src/main/java/com/example/taximotoapp_backend/location/service/LocationService.java
@@ -1,8 +1,24 @@
 package com.example.taximotoapp_backend.location.service;
 
+import com.example.taximotoapp_backend.User.model.Chauffeur;
+import com.example.taximotoapp_backend.User.repository.ChauffeurRepository;
+import com.example.taximotoapp_backend.location.dto.LocationRequest;
+import com.example.taximotoapp_backend.location.response.LocationResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class LocationService {
+    private final ChauffeurRepository chauffeurRepository;
 
+    public LocationResponse updateLocation(LocationRequest locationRequest){
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        Chauffeur chauffeur=chauffeurRepository.findByEmail(email).orElseThrow(()-> new RuntimeException("Chauffeur not found"));
+        chauffeur.setCurrent_latitude(locationRequest.getLatitude());
+        chauffeur.setCurrent_longitude(locationRequest.getLongitude());
+        chauffeurRepository.save(chauffeur);
+        return new LocationResponse(locationRequest.getLatitude(),locationRequest.getLongitude(),"Location updated successfully");
+    }
 }
