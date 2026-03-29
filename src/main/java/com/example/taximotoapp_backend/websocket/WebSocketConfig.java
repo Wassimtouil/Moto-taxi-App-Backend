bdef88a50b7adeb7244bb8750cc8bd3c0db8210c
Wassim Touil <153998360+Wassimtouil@users.noreply.github.com>
2026-03-29 23:04:25 +0200

    update WebSocketConfig

--- src/main/java/com/example/taximotoapp_backend/websocket/WebSocketConfig.java
+++ src/main/java/com/example/taximotoapp_backend/websocket/WebSocketConfig.java
@@ -25,7 +25,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
                 .addInterceptors(jwtInterceptor)  // sécurité JWT
-                .setAllowedOriginPatterns("http://localhost:5500") // frontend dev
+                .setAllowedOriginPatterns("*") // frontend dev
                 .withSockJS();
     }
 
