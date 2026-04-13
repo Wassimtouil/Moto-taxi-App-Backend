aba493491d8119ffe13ed3af3ab53b22d8c815e2
Wassim Touil <153998360+Wassimtouil@users.noreply.github.com>
2026-04-13 22:31:33 +0200

    add ReclamationController

---
+++ src/main/java/com/example/taximotoapp_backend/reclamation/controller/ReclamationController.java
@@ -0,0 +1,26 @@
+package com.example.taximotoapp_backend.reclamation.controller;
+
+import com.example.taximotoapp_backend.reclamation.dto.request.ReclamationRequest;
+import com.example.taximotoapp_backend.reclamation.dto.response.ReclamationResponse;
+import com.example.taximotoapp_backend.reclamation.service.ReclamationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/reclamation")
+public class ReclamationController {
+    private final ReclamationService service;
+
+
+    @PreAuthorize("hasAnyRole('CLIENT','CHAUFFEUR')")
+    @PostMapping("/createReclamation")
+    public ResponseEntity<ReclamationResponse> create(@RequestBody ReclamationRequest request) {
+        return ResponseEntity.ok(service.create(request));
+    }
+}
