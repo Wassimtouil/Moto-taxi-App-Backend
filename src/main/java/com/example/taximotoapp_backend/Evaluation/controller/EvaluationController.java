6934f7176e3905d58f3b71b1517a5cf87eb811d4
Wassim Touil <153998360+Wassimtouil@users.noreply.github.com>
2026-04-24 18:22:02 +0200

    update SecurityConfig for autorisation

---
+++ src/main/java/com/example/taximotoapp_backend/Evaluation/controller/EvaluationController.java
@@ -0,0 +1,12 @@
+package com.example.taximotoapp_backend.Evaluation.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/evaluation")
+public class EvaluationController {
+
+}
