4872f4e6f8dbf15b398cb6c35a9acde51975d85a
Wassim Touil <153998360+Wassimtouil@users.noreply.github.com>
2026-04-25 10:25:53 +0200

    update EvaluationRepository

--- src/main/java/com/example/taximotoapp_backend/Evaluation/Repository/EvaluationRepository.java
+++ src/main/java/com/example/taximotoapp_backend/Evaluation/Repository/EvaluationRepository.java
@@ -2,6 +2,8 @@ package com.example.taximotoapp_backend.Evaluation.Repository;
 
 import com.example.taximotoapp_backend.Evaluation.model.Evaluation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,4 +11,7 @@ import java.util.Optional;
 public interface EvaluationRepository extends JpaRepository<Evaluation,Long> {
     Optional<Evaluation> findByTrajetId(Long trajetId);
     List<Evaluation> findByChauffeurId(Long chauffeurId);
+
+    @Query("select avg(e.note) from Evaluation e where e.chauffeur.id= :id")
+    Double getMoyenne(@Param("id") Long id);
 }
