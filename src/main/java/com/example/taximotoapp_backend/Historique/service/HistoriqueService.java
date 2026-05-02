6bfdce48f4c52a2b9de046bffc1e4813cdd6398d
Wassim Touil <153998360+Wassimtouil@users.noreply.github.com>
2026-05-02 21:32:36 +0200

    update function getHistoriqueClient

--- src/main/java/com/example/taximotoapp_backend/Historique/service/HistoriqueService.java
+++ src/main/java/com/example/taximotoapp_backend/Historique/service/HistoriqueService.java
@@ -1,13 +1,17 @@
 package com.example.taximotoapp_backend.Historique.service;
 
+import com.example.taximotoapp_backend.Historique.dto.response.HistoriqueClientResponse;
+import com.example.taximotoapp_backend.Historique.mapper.HistoriqueMapper;
 import com.example.taximotoapp_backend.User.model.Chauffeur;
 import com.example.taximotoapp_backend.User.model.Client;
+import com.example.taximotoapp_backend.User.model.User;
 import com.example.taximotoapp_backend.User.repository.ChauffeurRepository;
 import com.example.taximotoapp_backend.User.repository.ClientRepository;
 import com.example.taximotoapp_backend.User.repository.UserRepository;
 import com.example.taximotoapp_backend.trajet.model.Trajet;
 import com.example.taximotoapp_backend.trajet.repository.TrajetRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,17 +22,20 @@ public class HistoriqueService {
     private final TrajetRepository trajetRepository;
     private final ClientRepository clientRepository;
     private final ChauffeurRepository chauffeurRepository;
+    private final UserRepository userRepository;
+    private final HistoriqueMapper mapper;
 
-    public List<Trajet> getHistoriqueClient(Long clientId) {
-        Client client = clientRepository.findById(clientId)
-                .orElseThrow(() -> new RuntimeException("Client introuvable"));
+    public List<HistoriqueClientResponse> getHistoriqueClient() {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new RuntimeException("User introuvable"));
+        if (!(user instanceof Client client)) {
+            throw new RuntimeException("Accès refusé : utilisateur non client");
+        }
+        return client.getTrajets()
+                .stream()
+                .map(mapper::toHistoriqueClientResponse)
+                .toList();
+    }
 
-        return client.getTrajets();
-    }
-    public List<Trajet> getHistoriqueChauffeur(Long chauffeurId) {
-        Chauffeur chauffeur = chauffeurRepository.findById(chauffeurId)
-                .orElseThrow(() -> new RuntimeException("Chauffeur introuvable"));
-        return chauffeur.getTrajets();
-    }
 
 }
