38e4fcaae75ad1721d4e1010cc6d7160dde9c5d6
Wassim Touil <153998360+Wassimtouil@users.noreply.github.com>
2026-05-17 20:44:41 +0200

    add dtos

---
+++ src/main/java/com/example/taximotoapp_backend/poi/dto/response/PoiResponse.java
@@ -0,0 +1,11 @@
+package com.example.taximotoapp_backend.poi.dto.response;
+
+public class PoiResponse {
+    private Long id;
+    private String name;
+    private String category;
+    private String city;
+    private Double latitude;
+    private Double longitude;
+    private String address;
+}
