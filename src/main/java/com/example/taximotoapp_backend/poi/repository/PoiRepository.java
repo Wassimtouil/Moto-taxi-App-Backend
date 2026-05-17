e6689b7d5e53f720cff7336dd615b45b4148a0cf
Wassim Touil <153998360+Wassimtouil@users.noreply.github.com>
2026-05-17 20:34:30 +0200

    add PoiRepository for search

---
+++ src/main/java/com/example/taximotoapp_backend/poi/repository/PoiRepository.java
@@ -0,0 +1,17 @@
+package com.example.taximotoapp_backend.poi.repository;
+
+import com.example.taximotoapp_backend.poi.model.Poi;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface PoiRepository {
+    @Query("""
+        SELECT p FROM Poi p
+        LEFT JOIN PoiSynonym s ON s.poi = p
+        WHERE LOWER(p.name) = LOWER(:text)
+        OR LOWER(s.synonym) = LOWER(:text)
+    """)
+    List<Poi> search(@Param("text") String text);
+}
