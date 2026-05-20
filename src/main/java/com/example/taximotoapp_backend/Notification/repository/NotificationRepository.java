0c03112b77fd7d6a3392dabb5cf45e5bb7aab13a
Wassim Touil <153998360+Wassimtouil@users.noreply.github.com>
2026-05-20 16:41:27 +0200

    add NotificationRepository

---
+++ src/main/java/com/example/taximotoapp_backend/Notification/repository/NotificationRepository.java
@@ -0,0 +1,13 @@
+package com.example.taximotoapp_backend.notification.repository;
+
+import com.example.taximotoapp_backend.notification.model.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findAllByOrderByCreatedAtDesc();
+    long countByIsReadFalse();
+}
