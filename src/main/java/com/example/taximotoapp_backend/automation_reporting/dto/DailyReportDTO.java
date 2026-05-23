fff704182cd5118c3705c376e198daa09a61ff78
Wassim Touil <153998360+Wassimtouil@users.noreply.github.com>
2026-05-23 18:30:26 +0200

    create package automation_reporting,add dto

---
+++ src/main/java/com/example/taximotoapp_backend/automation_reporting/dto/DailyReportDTO.java
@@ -0,0 +1,25 @@
+package com.example.taximotoapp_backend.automation_reporting.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Map;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DailyReportDTO {
+    private Double todayRevenue;
+    private Long totalTrips;
+    private Double cancellationRate;
+    private Long newClients;
+    private Long inactiveDrivers;
+    private List<TopDriverDTO> topDrivers;
+    private PaymentStatsDTO paymentStats;
+    private List<Map<String, Object>> reclamations;
+    private String aiAnalysis;
+}
